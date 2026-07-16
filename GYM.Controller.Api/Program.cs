7731ca8ee07292b65cc194fa5aaa67765c3fcf34
StefanoHA <stefano.herrejon@hotmail.com>
2026-07-15 22:59:06 -0600

    Se añadio serilog al programa

--- GYM.Controller.Api/Program.cs
+++ GYM.Controller.Api/Program.cs
@@ -1,6 +1,15 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console() // Write to console, and write to a file - starting a new file each day.
+    .WriteTo.File("logs/fulfillment-log-.log", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog(); // Tell the builder to use Serilog for logging
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -24,6 +33,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+//app.UseSerilogRequestLogging(); No se cuando hay que descomentar
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
@@ -31,3 +42,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+Log.CloseAndFlush(); // Close and flush the logs (serilog)
