80e5298581cdd8679b820a3c846107b1881891ec
StefanoHA <stefano.herrejon@hotmail.com>
2026-07-15 22:55:53 -0600

    Añadi logica de constraints, y comentario

--- GYM.Data/Entities/Room.cs
+++ GYM.Data/Entities/Room.cs
@@ -7,5 +7,5 @@ public class Room
     public int Id {get; set; }
     public string Name {get; set;} = ""; //Official name of the room
     public int MaxCapacity {get; set; } //Maximmum capacity for the room
-    public bool IsAvailable {get; set;} //IS the room available ? yes : no
+    public bool IsAvailable {get; set;} //IS the room available. Not based on other classes but on external circumstances
 }
--- GYM.Data/GymDbContext.cs
+++ GYM.Data/GymDbContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using GYM.Data.Entities;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace GYM.Data;
 
@@ -23,6 +24,70 @@ public class GymDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         //Falta constraints
+        modelBuilder.Entity<Room>(e =>
+        {
+            //id : unico, obligatorio, autoincremental
+            //name : max 100 length, obligatorio
+            //maxCapacity : 0 < capacidad < 35
+            //isAvailable default = true
+        });
+
+        modelBuilder.Entity<Training>(e =>
+        {
+            //id unico, antoincremental, obligatorio
+            //instructorid, es obligatorio - relación de 1 instructor por training
+            //Descripcion maxLength 250 caracteres
+            //ClassStart < Classend
+            //RoomID FK, relación de n training a 1 room
+        });
+
+        modelBuilder.Entity<Booking>(e =>
+        {
+            //id unico, antoincremental, obligatorio
+            //0<=totalAmount < 35/maxCapacitydDeROom
+            //PaymentID FK- relacion. 1 booking a 1 payment
+            //BookAt - hora actual
+        });
+
+        modelBuilder.Entity<Payment>(e =>
+        {
+            //id unico, antoincremental, obligatorio
+            //0 < totalAmount < MaxDecimal, precision ? decimales 2
+            //PAymentType ? 
+            //Status enum
+            //PaymentDate - hora actual
+        });
+
+        modelBuilder.Entity<User>(e =>
+        {
+            //id unico, antoincremental, obligatorio
+            //email, type email, obligatorio, unico
+            //phone, unico, obligatorio, maxLength 15
+            //password -> Hashed, MaxLength ???
+            //Role obligatorio
+        });
+
+        modelBuilder.Entity<UserDetail>(e =>
+        {
+            //id unico, antoincremental, obligatorio
+            //userId FK - 1 a 1
+            //name maxLenth 50, obligatorio
+            //surname maxLength 50
+            //join at - hora de creación
+        });
+
+        modelBuilder.Entity<Statistic>(e =>
+        {
+            //id unico, antoincremental, obligatorio
+            //UserId - 1 userId a n statistic
+            // 0 < Weight  < MaxDecimal, precision ? decimales 2. obligatorio
+            // 0 < maxHeight  < MaxDecimal, precision ? decimales 2. obligatorio
+            // 0 < strength  < MaxDecimal, precision ? decimales 2. obligatorio
+            //0'0'' < milerun < max ?. obligatorio
+            //Measureat - al crear, obligatorio
+
+        });
+
     }
 
 
