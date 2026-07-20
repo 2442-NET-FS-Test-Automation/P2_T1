b18d6f64d9c42aecdf48beaa4672e1c0ddca2ad2
MiguelAranda64 <0321101236@ut-tijuana.edu.mx>
2026-07-20 11:35:41 -0600

    created react project, new Home web moduke, added some config to Program.cs

--- GYM.Controller.Api/Program.cs
+++ GYM.Controller.Api/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using GYM.Data.Entities;
 using Serilog;
+using GYM.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// remember to change the connection string depending on your configuration
+// miguel pass: libraryPass1!
+var conn_string = "Server=localhost,1433;Database=LibraryMinimalDb;User Id=sa;Password=libraryPass1!;TrustServerCertificate=true";
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console() // Write to console, and write to a file - starting a new file each day.
@@ -10,6 +16,18 @@ Log.Logger = new LoggerConfiguration()
     .CreateLogger();
 
 builder.Host.UseSerilog(); // Tell the builder to use Serilog for logging
+
+// Adding CORS 
+const string SpaCorsPolicy = "spa"; // string name for our policy
+
+// Configuring our CORS policy
+builder.Services.AddCors(o => o.AddPolicy(SpaCorsPolicy, p => p
+    .WithOrigins("http://127.0.0.1:5137","http://127.0.0.1:5500")
+    .AllowAnyHeader()
+    .AllowAnyMethod()    
+));
+
+builder.Services.AddDbContextFactory<GymDbContext>(o => o.UseSqlServer(conn_string));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
--- GYM.FrontEnd/src/main.tsx
+++ GYM.FrontEnd/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './Home' // <-- Aquí importas tu componente (ajusta la ruta si está en otra carpeta)
+import 'bootstrap/dist/css/bootstrap.min.css' // Si vas a usar Bootstrap
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Home /> {/* <-- Aquí cambias lo que venía por tu componente Main */}
+  </React.StrictMode>,
 )
