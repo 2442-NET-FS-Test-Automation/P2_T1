647fde0675c2101db95f33b52b85b6d59de94491
carlosotero2002 <carlos.otero@edu.uag.mx>
2026-07-25 10:53:10 -0600

    UPDATE/DTO change

--- GYM.Controller.Api/DTOs/BookingDTO.cs
+++ GYM.Controller.Api/DTOs/BookingDTO.cs
@@ -3,11 +3,11 @@ using GYM.Data.Entities;
 namespace GYM.Controller.Api.DTOs;
 
 public class BookingDTO{
-    public int Id;
-    public int TrainingId;
-    public int UserId;
+    public int Id {get;set;}
+    public int TrainingId {get;set;}
+    public int UserId {get;set;}
     public BookingStatus Status { get; set; } = BookingStatus.Booked;
-    public DateTime? ExerciseTime;
-    public DateTime? DoneAt;
+    public DateTime? ExerciseTime {get;set;} 
+    public DateTime? DoneAt {get;set;} 
 };
 
