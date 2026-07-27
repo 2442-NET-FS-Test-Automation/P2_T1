238c55176f1e59d72c0f022242286d1361d2071b
MiguelAranda64 <0321101236@ut-tijuana.edu.mx>
2026-07-26 22:45:35 -0600

    user booking page now working

--- GYM.FrontEnd/src/services/BookingService.ts
+++ GYM.FrontEnd/src/services/BookingService.ts
@@ -9,6 +9,20 @@ export interface CreateBookingDTO {
   exerciseTime: string;
 }
 
+// public enum BookingStatus { Booked, Working, Completed, Cancelled }
+const STATUS_TO_NUMBER: Record<BookingStatus, number> = {
+  Booked: 0,
+  Working: 1,
+  Completed: 2,
+  Cancelled: 3,
+};
+
+// Convierte "00:45:00" a un DateTime completo tipo "2026-07-27T00:45:00"
+const toDateTimeString = (time: string): string => {
+  const todayISO = new Date().toISOString().split("T")[0]; // "2026-07-27"
+  return `${todayISO}T${time}`;
+};
+
 // get all bookings
 export const getPublicBookings = async (): Promise<BookingDTO[]> => {
   try {
@@ -25,7 +39,13 @@ export const createBooking = async (
 booking: CreateBookingDTO
 ): Promise<BookingDTO | null> => {
   try {
-    const response = await api.post<BookingDTO>('/Booking/bookings', booking);
+    const payload = {
+      ...booking,
+      status: STATUS_TO_NUMBER[booking.status],
+      exerciseTime: toDateTimeString(booking.exerciseTime)
+    }
+
+    const response = await api.post<BookingDTO>('/Booking/bookings', payload);
     return response.data
   } catch (error) {
     console.error('Error creating booking:', error);
