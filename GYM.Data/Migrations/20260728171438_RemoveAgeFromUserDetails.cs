3da8f0bd5b93782bbbbbc587b16b9ef3da6a3c91
MiguelAranda64 <0321101236@ut-tijuana.edu.mx>
2026-07-28 20:41:31 -0600

    pls ignore

---
+++ GYM.Data/Migrations/20260728171438_RemoveAgeFromUserDetails.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GYM.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveAgeFromUserDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "UserDetails");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "UserDetails",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
--- GYM.FrontEnd/src/Pages/admin/AdminTrainingsPage.tsx
+++ GYM.FrontEnd/src/Pages/admin/AdminTrainingsPage.tsx
@@ -6,6 +6,7 @@ import { ViewExercisesModal } from '../../Components/Admin/modals/ViewExercisesM
 import type { TrainingDTO, TrainingCreateDTO } from '../../types/trainingDTO';
 import type { exerciseDTO } from '../../types/exerciseDTO';
 import {Pagination} from '../../Components/Pagination';
+import { toast } from 'react-toastify';
 
 export const AdminTrainingsPage: React.FC = () => {
     const [trainings, setTrainings] = useState<TrainingDTO[]>([]);
@@ -43,7 +44,7 @@ export const AdminTrainingsPage: React.FC = () => {
         setAvailableExercises(exercisesData);
         } catch (error) {
         console.error('Error loading trainings or exercises:', error);
-        alert('Error al cargar la información de rutinas desde el servidor.');
+        toast.error('Error al cargar la información de rutinas desde el servidor.');
         } finally {
         setLoading(false);
         }
@@ -80,7 +81,7 @@ export const AdminTrainingsPage: React.FC = () => {
             loadData(); 
         } catch (error) {
             console.error('Error saving training:', error);
-            alert('an error occurred while saving the training. Please try again.');
+            toast.error('an error occurred while saving the training. Please try again.');
         }
     };
 
@@ -94,7 +95,7 @@ export const AdminTrainingsPage: React.FC = () => {
         loadData();
         } catch (error) {
         console.error('Error deleting training:', error);
-        alert('No se pudo eliminar el entrenamiento.');
+        toast.error("No se pudo eliminar el entrenamiento.");
         }
     };
 
