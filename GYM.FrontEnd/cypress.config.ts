f0818b7453ef4d8e3ba785e4110fd28e723af477
StefanoHA <stefano.herrejon@hotmail.com>
2026-08-03 18:46:40 -0600

    Ya funciona cypress por componenete

--- GYM.FrontEnd/cypress.config.ts
+++ GYM.FrontEnd/cypress.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
     baseUrl: "http://localhost:5173", //Url de REACT -> FrontEnd
     supportFile: "cypress/support/e2e.ts" //Codigo disponible para todas las pruebas/tests
   },
+  component:{
+    devServer:{
+      framework: "react",
+      bundler: "vite",
+    }
+  }
 });
