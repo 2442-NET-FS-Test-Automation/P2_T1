1aeb24862aedfa99a6adc48cc9391efe844fad11
StefanoHA <stefano.herrejon@hotmail.com>
2026-08-15 15:48:53 -0600

    Auth test funciona

--- GYM.Tests/Tests.Integration/GymApiFactory.cs
+++ GYM.Tests/Tests.Integration/GymApiFactory.cs
@@ -1,8 +1,15 @@
 using GYM.Controller.Api;
+using GYM.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using GYM.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+
 
 namespace GYM.Tests.Tests.Integration;
 
@@ -13,9 +20,10 @@ public class GymApiFactory : WebApplicationFactory<Program>
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        
+        base.ConfigureWebHost(builder);
     }
 }
+
 
 
 //Una factory para los test.
